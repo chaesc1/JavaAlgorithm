3c498758f93692f2f49a5f0b1906c70f8f08476c
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-09-27 02:13:01 +0900

    2차원 배열 인덱스 계산 로직

--- src/Programmers/level2/P02.java
+++ src/Programmers/level2/P02.java
@@ -1,2 +1,34 @@
-package Programmers.level2;public class P02 {
+package Programmers.level2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P02 {
+    public static void main(String[] args) {
+        int n = 4;
+        long left = 7,right = 14;
+        int len = (int)right - (int)left; //2 3 4 5 -> 4개
+        int[] arr = new int[len + 1];
+        int[][] arr2 = new int[n+1][n+1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr2[i][j] = Math.max(i,j)+1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr2[i][j]);
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int row = (int)((i+left)/n) + 1;
+            int col = (int)((i+left)%n) + 1;
+            arr[i] = Math.max(row,col);
+
+            System.out.println(arr[i]);
+        }
+    }
 }

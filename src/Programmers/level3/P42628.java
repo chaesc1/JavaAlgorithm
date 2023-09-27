afbaff9ad8e15cd2f5b305a75926aa5294e9e4d5
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-09-27 11:01:54 +0900

    이중우선순위큐

---
+++ src/Programmers/level3/P42628.java
@@ -0,0 +1,16 @@
+package Programmers.level3;
+//이중우선순위큐
+
+class Solution42682 {
+    public int[] solution(String[] operations) {
+        int[] answer = {};
+        return answer;
+    }
+}
+public class P42628 {
+    public static void main(String[] args) {
+        Solution42682 s = new Solution42682();
+        String[] oper = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+        s.solution(oper);
+    }
+}

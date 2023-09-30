a9a19c1d38b9233d33b8e4f940d008a94aebc787
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-09-30 16:13:07 +0900

    P43162 DFS풀이

--- src/Programmers/level3/P43162.java
+++ src/Programmers/level3/P43162.java
@@ -1,2 +1,41 @@
-package Programmers.level3;public class P43162 {
+package Programmers.level3;
+//1 1 0
+//1 1 0
+//0 0 1
+
+//1 1 0
+//1 1 1
+//0 1 1
+
+class Solution43162 {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] check = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if(!check[i]){
+                dfs(i,check,computers);//start,check,computers
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    private void dfs(int i, boolean[] check, int[][] computers) {
+        check[i] = true;//시작점 check를 true로
+        for (int j = 0; j < computers.length; j++) {
+            if(computers[i][j] == 1 && !check[j]){
+                dfs(j,check,computers);
+            }
+        }
+    }
 }
+public class P43162 {
+    public static void main(String[] args) {
+        Solution43162 s =  new Solution43162();
+        int n = 3;
+        int[][] computers = {{1,1,0},{1,1,0},{0,0,1}};
+        System.out.println( s.solution(n,computers));
+    }
+}
+

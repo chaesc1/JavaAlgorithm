940e5a2105c11e1261952ac47bf983191c2262a2
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-01-10 20:01:23 +0900

    java CodingTest

--- src/BOJ/BackTracking/P15666.java
+++ src/BOJ/BackTracking/P15666.java
@@ -5,10 +5,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class P15663 {
+public class P15666 {
     static int[] arr;
     static int[] printArr;
-    static boolean[] visited;
     static int N, M;
     static StringBuilder sb = new StringBuilder();
 
@@ -22,6 +21,7 @@ public class P15663 {
 
         arr = new int[N];
         printArr = new int[N];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
@@ -29,10 +29,9 @@ public class P15663 {
         Arrays.sort(arr);
         dfs(0,0);
         System.out.print(sb);
-
     }
 
-    private static void dfs(int start,int depth) {
+    private static void dfs(int start, int depth) {
         if (depth == M) {
             for (int i = 0; i < M; i++) {
                 sb.append(printArr[i]).append(" ");
@@ -40,14 +39,14 @@ public class P15663 {
             sb.append("\n");
             return;
         }
-        int back = -1;//이전 숫자
 
-        for (int i = 0; i < N; i++) {
+        int back = -1;//이전 숫자
+        for (int i = start; i < N; i++) {
             int now = arr[i];
             if (now != back) {
                 back = now;
-                printArr[depth] = now;
-                dfs(0,depth+1);
+                printArr[depth] = arr[i];
+                dfs(i,depth+1);
             }
         }
     }
---
+++ src/BOJ/DFSBFS/P11725.java
@@ -0,0 +1,54 @@
+package BOJ.DFSBFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class P11725 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        //그래프 초기화
+        for (int i = 0; i < N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < N-1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken())-1;
+            int end = Integer.parseInt(st.nextToken())-1;
+
+            graph.get(start).add(end);
+            graph.get(end).add(start);
+        }
+
+        boolean[] visited = new boolean[N];
+        int[] parent = new int[N];
+
+        //BFS
+        Queue<Integer> queue = new LinkedList<>();
+        visited[0] = true;
+        queue.add(0);
+
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            for (int node : graph.get(num)) {
+                if (!visited[node]) {
+                    visited[node] = true;
+                    queue.add(node);
+                    parent[node] = num;
+                }
+            }
+        }
+
+        for (int i = 1; i < N; i++) {
+            System.out.println(parent[i]+1);
+        }
+    }
+}

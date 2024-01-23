4792a00b7356d023acbc65ead3aa6c3e74eb73a5
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-01-24 00:40:59 +0900

    Prim 알고리즘

--- DataStructure/src/PrimAlgo.java
+++ DataStructure/src/PrimAlgo.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class PrimAlgo {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Edge implements Comparable<Edge> {
+    int w;
+    int cost;
+
+    Edge(int w, int cost) {
+        this.w = w;
+        this.cost = cost;
+    }
+
+    //간선을 오름차순으로 정렬
+    @Override
+    public int compareTo(Edge edge) {
+        return this.cost - edge.cost;
+    }
+}
+public class PrimAlgo {
+
+    //최소 신장 트리를 찾기 위한 그리디 패러다임 알고리즘
+
+    //Step1 : 임의의 정점을 선택하여 비어있는 T에 포함 - T는 한 개인의 트리
+    //Step2 : T 에 있는 노드와 T 에 없는 노드 사이의 간선 중 가중치가 최소인 간선을 찾는다
+    //Step3 : 찾은 간선이 연결하는 두 노드 중, T 에 없던 노드를 T에 포함시킨다 (step 1에서 찾은 간선도 같이 T에 포함됩니다.)
+    //Step4 : 모든 노드가 T에 포함될때 까지 2,3을 반복
+
+    //간선 저장을 위한 클래스
+    static List<Edge> [] graph;
+    public static void prim(int start, int n) {
+        boolean[] visited = new boolean[n + 1];
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start,0));
+
+        int total = 0;
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int v = edge.w;
+            int cost = edge.cost;
+
+            if (visited[v]) continue; //방문표시된 노드면 뛰어넘고
+
+            visited[v] = true;
+            total += cost;
+
+            for (Edge e : graph[v]) {
+                if (!visited[e.w]) {
+                    pq.add(e);
+                }
+            }
+        }
+        System.out.println(total);
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        //그래프 선언하고 간선 리스트로 표현
+        graph = new ArrayList[n+1];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[v].add(new Edge(w,cost));
+            graph[w].add(new Edge(v,cost));
+        }
+
+        prim(1,n);
+    }
 }
--- DataStructure/src/UnionFind.java
+++ DataStructure/src/UnionFind.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class UnionFind {
+//그래프 트리 대표적인 알고리즘
+// 서로소인 부분집합의 표현
+
+import static java.nio.file.Files.find;
+
+public class UnionFind {
+    static int[] parent;
+    public static void main(String[] args) {
+        int n = 5;
+        parent = new int[n+1];
+
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+        }
+
+        union(1,2);
+        parentPrint();
+        union(3,4);
+        parentPrint();
+        union(3,5);
+        parentPrint();
+
+        System.out.println("find(2) = " + find(2));
+        System.out.println("find(4) = " + find(4));
+        union(2,4);
+        parentPrint();
+    }
+    public static void parentPrint() {
+        System.out.print("[ ");
+        for (int i = 1; i < parent.length; i++) {
+            System.out.print(parent[i] + " ");
+        }
+        System.out.println("]");
+    }
+    public static boolean union(int x,int y) {
+        x = find(x); //x's parent
+        y = find(y);//y's parent
+
+        //이미 같은 그래프에 속해있을 때 false 반환
+        if(x == y) return false;
+
+        if (x <= y) parent[y] = x;
+        else parent[x] = y;
+
+        return true;
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        return find(parent[x]);
+    }
+
 }
+

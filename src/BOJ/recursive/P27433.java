0090b359a9fed51319abcfa266b0f771217417cb
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-02-10 00:35:31 +0900

    Recursive

---
+++ src/BOJ/recursive/P27433.java
@@ -0,0 +1,21 @@
+package BOJ.recursive;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P27433 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(n));
+    }
+
+    private static long factorial(int n) {
+        if (n==0 || n==1) {
+            return 1;
+        } else {
+            return n * factorial(n-1);
+        }
+    }
+}

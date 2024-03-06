6d1c342db5a243afad2d06a972af050c61028cc2
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:10:38 +0900

    [Silver II] Title: 스택 수열, Time: 1104 ms, Memory: 115020 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args)throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int A[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+        Stack<Integer> stack = new Stack<>();
+        int num = 1;
+        boolean result = true;
+        StringBuffer bf = new StringBuffer();
+        for (int i = 0; i < A.length; i++) {
+            int temp = A[i];
+            if(temp >= num){
+                while (temp >= num){
+                    stack.push(num++);
+                    bf.append("+\n");
+                }
+                stack.pop();
+                bf.append("-\n");
+            }else{
+                int temp2 = stack.pop();
+                if(temp2 > temp){
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                }
+                else{
+                    bf.append("-\n");
+                }
+            }
+        }
+        if(result) System.out.println(bf.toString());
+    }
+}

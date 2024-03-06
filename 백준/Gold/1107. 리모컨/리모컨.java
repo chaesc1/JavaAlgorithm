0b0e03ecccc5a17a95c12e30230a39b6428859e0
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:08:21 +0900

    [Gold V] Title: 리모컨, Time: 444 ms, Memory: 213728 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] isPossible;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); //이동하려는 채널
+        int M = Integer.parseInt(br.readLine()); //고장난 버튼 개수
+
+        isPossible = new boolean[10];
+        if ( M > 0 ){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                isPossible[num] = true; // 못누르게 체크표시.
+            }
+        }
+
+        int remain = Math.abs(N - 100);
+        for (int i = 0; i <= 999999; i++) {
+            String input = String.valueOf(i);
+            int len = input.length(); //99 라면 2번 누른거
+
+            boolean isBroken = false;//고장났는지 판별
+            for (int j = 0; j < len; j++) {
+                if (isPossible[Integer.parseInt(String.valueOf(input.charAt(j)))]) {
+                    isBroken = true;
+                    break;
+                }
+            }
+
+            if (!isBroken) {
+                int min = Math.abs(N - i) + len;
+                remain = Math.min(remain,min);
+            }
+        }
+
+        System.out.println(remain);
+    }
+}

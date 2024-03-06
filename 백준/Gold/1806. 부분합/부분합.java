48a884d0b3086b8d3250875822a1fd8970c96849
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:10:34 +0900

    [Gold IV] Title: 부분합, Time: 292 ms, Memory: 24216 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //투포인터 시작
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int len = Integer.MAX_VALUE;
+
+        while (start <= end && end <= n) {
+            if (sum < s) {
+                sum += arr[end++];
+            } else if (sum >= s) {
+                len = Math.min(len, end - start);
+                sum -= arr[start++];
+            }
+        }
+
+        if (len == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(len);
+        }
+    }
+}

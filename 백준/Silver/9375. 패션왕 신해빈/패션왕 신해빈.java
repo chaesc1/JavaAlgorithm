c473c76f8537ca72514a0351e5d1c0461f478c3a
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:13:58 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 136 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int tc = Integer.parseInt(br.readLine());
+
+
+
+        for (int i = 0; i < tc; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            HashMap<String,Integer> map = new HashMap<>(); //key 의상 종류 ,
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                String name = st.nextToken();
+                String type = st.nextToken();
+
+                map.put(type,map.getOrDefault(type,0)+1);
+            }
+
+            int result = 1;
+            for(int count : map.values()) {
+                result *= (count+1);
+            }
+            System.out.println(result-1);
+
+        }
+    }
+}

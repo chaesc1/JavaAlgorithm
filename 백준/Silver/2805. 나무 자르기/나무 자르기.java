104da493c863cfe7741e01ea78f3069745728e27
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:12:45 +0900

    [Silver II] Title: 나무 자르기, Time: 536 ms, Memory: 119404 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long max = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+
+        long start = 0;
+        long end = max;
+
+        while (start < end) {
+            long mid = (start + end) / 2;
+            long sum = 0;
+
+            for (int tree : arr) {
+                if (tree > mid) {
+                    sum += (tree - mid);
+                }
+            }
+            //원하는 길이보다 짧다면
+            if (sum >= M){
+                start = mid+1;
+            } else {
+                end = mid;
+            }
+            
+        }
+        System.out.println(end-1);
+    }
+}

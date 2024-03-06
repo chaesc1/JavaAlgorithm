306f671552df60bea09eb34b5e4effcf62855941
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:16:17 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 664 ms, Memory: 49756 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] arr, distance;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static int n, m;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        distance = new int[n][m];
+        visited = new boolean[n][m];
+        int startX = 0;
+        int startY = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 2) {
+                    startX = i;
+                    startY = j;
+                } else if (arr[i][j] == 0) {
+                    visited[i][j] = true;
+                }
+            }
+        }
+        bfs(startX, startY);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j]) {
+                    distance[i][j] = -1;
+                }
+                sb.append(distance[i][j]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] pos = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = pos[0] + dx[i];
+                int ny = pos[1] + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (!visited[nx][ny] && arr[nx][ny] == 1) {
+                        q.add(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                        distance[nx][ny] = distance[pos[0]][pos[1]] + 1;
+                    }
+                }
+            }
+        }
+    }
+}

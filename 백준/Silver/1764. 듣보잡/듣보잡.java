52b4728645c5b750b8c89be3980de8ee4d16b089
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:10:24 +0900

    [Silver IV] Title: 듣보잡, Time: 324 ms, Memory: 25988 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            map.put(br.readLine(), 0);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            String input = br.readLine();
+            if (map.get(input) != null) {
+                list.add(input);
+            }
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.size());
+        for(String s : list) {
+            System.out.println(s);
+        }
+    }
+}

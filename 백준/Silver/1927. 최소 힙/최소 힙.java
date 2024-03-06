7ef4a1b84a2622ee43b6eddab85a1d262f9540e5
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:10:52 +0900

    [Silver II] Title: 최소 힙, Time: 336 ms, Memory: 26272 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            int command = Integer.parseInt(br.readLine());
+            if (command == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0);
+                } else {
+                    sb.append(pq.poll());
+                }
+                sb.append("\n");
+            } else {
+                pq.offer(command);
+            }
+        }
+        br.close();
+        System.out.println(sb);
+    }
+}

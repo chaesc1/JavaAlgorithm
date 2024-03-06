45438ae6625ea9722b2b65ba4f42755b003fa83b
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:12:20 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int num;
+    static int count = 0;
+    static int[][] arr;
+    static boolean[] check;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        num = Integer.parseInt(br.readLine());
+        int line = Integer.parseInt(br.readLine());
+
+        arr = new int[num+1][num+1];
+        check = new boolean[num+1];
+        for (int i = 0; i < line; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            arr[from][to] = arr[to][from] = 1;
+        }
+
+        dfs(1);
+        System.out.println(count);
+    }
+
+    public static void dfs(int start) {
+        check[start] = true;
+
+        for (int i = 0; i <= num; i++) {
+            if (!check[i] && arr[start][i] == 1) {
+                count++;
+                dfs(i);
+            }
+        }
+    }
+}

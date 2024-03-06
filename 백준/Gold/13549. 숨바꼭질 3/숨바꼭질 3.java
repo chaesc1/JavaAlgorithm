b19a97803a1eb241c42aef1cb82372b5277b5624
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:15:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 19492 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+//숨바꼭질3 - gold3
+// 순간이동 (*2 ) -> 0 초
+// 1 앞으로 -> 1초 , 1 뒤로 -> 1초
+
+public class Main {
+    //가장 빠른 시간을 구해야해.
+    static int[] dx = {-1, 1};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        bfs(N, K);
+    }
+
+    public static void bfs(int N, int K) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{N, 0});
+
+        boolean[] isVisit = new boolean[100_001];
+        isVisit[N] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int position = cur[0];
+            int time = cur[1];
+
+            if (position == K) {
+                System.out.println(time);
+                return;
+            }
+            int warp = position * 2;// 순간이동한 위치 가중치가 2라서 제일 먼저 해줘야해
+            if (warp < 100001 && !isVisit[warp]) {
+                isVisit[warp] = true;
+                q.add(new int[] {warp,time}); // time = 0초라서 그대로
+            }
+            for (int i = 0; i < 2; i++) {
+                int nx = position + dx[i];
+                if(nx >= 0 && nx < 100001) {//범위 내
+                    //방문하지 않았다면
+                    if (!isVisit[nx]) {
+                        isVisit[nx] = true;
+                        q.add(new int[] {nx,time+1});
+                    }
+                }
+            }
+
+        }
+    }
+}

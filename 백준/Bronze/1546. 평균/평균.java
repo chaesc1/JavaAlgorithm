79d9a592a30e0440cd7a7afea10e298312a7b109
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:09:58 +0900

    [Bronze I] Title: 평균, Time: 264 ms, Memory: 19500 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double arr[] = new double[sc.nextInt()]; //배열 크기 지정, double 형으로 생성
+
+        for (int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        double sum = 0;
+        Arrays.sort(arr); //정렬 수행 마지막 원소가 M
+        double M = arr[arr.length-1];
+
+        for(int i=0; i<arr.length; i++){
+            sum+=arr[i];
+        }
+
+        System.out.println(sum/M*100/arr.length);
+    }
+}

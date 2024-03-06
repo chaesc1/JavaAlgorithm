0a5138de745d98815b0ddf0dd7bd2cc76e30ad67
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:10:29 +0900

    [Silver I] Title: 종이 접기, Time: 132 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Silver/1802. 종이 접기/종이 접기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//1은 위의 그림에서 OUT을 의미하고
+//0은 위의 그림에서 IN
+public class Main {
+
+    static String input;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            input = br.readLine();
+            if (check(0, input.length() - 1)) {
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean check(int start, int end) {
+        if (start == end) {
+            //길이가 1 인경우 true
+            return true;
+        }
+        int mid = (start + end) / 2;
+        for (int i = start; i < mid; i++) {
+            if (input.charAt(i) == input.charAt(end-i)) {
+                return false;
+            }
+        }
+        return check(start,mid-1) && check(mid+1,end);
+    }
+}

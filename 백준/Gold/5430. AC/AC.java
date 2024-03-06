9098f8f993714282f16cd665d10086020976ab8a
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-03-06 12:13:19 +0900

    [Gold V] Title: AC, Time: 852 ms, Memory: 84584 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Integer> deque;
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            String func = br.readLine(); //명령어
+            int n = Integer.parseInt(br.readLine()); // 배열 길이.
+
+            st = new StringTokenizer(br.readLine(),"[],");
+            deque = new ArrayDeque<>();
+
+            for (int j = 0; j < n; j++) {
+                deque.add(Integer.parseInt(st.nextToken())); //deque 에 삽입
+            }
+
+            System.out.println(AC(deque,func));
+        }
+    }
+
+    private static String AC(ArrayDeque<Integer> deque, String func) {
+        boolean isReverse = false;
+        for (char cmd : func.toCharArray()) {
+            if (cmd == 'R'){    //방향 바꿔줘
+                isReverse = !isReverse;
+            }
+            else {
+                if (deque.size() == 0) {
+                    return "error";
+                }
+
+                if (isReverse) {
+                    deque.removeLast();
+                } else {
+                    deque.removeFirst();
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder("[");
+        while (!deque.isEmpty()) {
+            if (isReverse) {
+                sb.append(deque.removeLast());
+            } else {
+                sb.append(deque.removeFirst());
+            }
+
+            if (deque.size() != 0) {
+                sb.append(",");
+            }
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+}

3caeedacf73e7ece510dca1e103edc63415adb31
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-04-12 01:30:27 +0900

    [level 2] Title: 올바른 괄호, Time: 20.59 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else {
+                if(stack.isEmpty() || stack.pop() == ')') {
+                    return false;
+                } 
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}

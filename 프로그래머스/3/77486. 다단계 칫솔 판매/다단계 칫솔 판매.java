a07981e20db508da01276ba7ef7355a207ec337f
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-04-24 17:58:29 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 41.12 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,45 +1,31 @@
 import java.util.*;
 
-/*
-민호는 center 
-각각의 자신은 본인의 부모 노드에게 연결된 트리구조
-판매에 의해 발생하는 이익의 10프로를 부모에게 배분, 나머지는 본인이 가짐
-1원 미만일 경우 이득 분배 X, 자신이 모두 가짐 
-*/
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        //referral 을 준 이유????????
-        HashMap<String,Integer> sellerMap = new HashMap<>(); // 인원 , 수익
-        HashMap<String,String> referralMap = new HashMap<>(); // 인원 , 수익
-        for(int i=0; i<enroll.length; i++) {
-            referralMap.put(enroll[i],referral[i]);
-            sellerMap.put(enroll[i],0);        
+        HashMap<String,String> parent = new HashMap<>();
+        
+        for(int i=0; i<referral.length; i++) {
+            parent.put(enroll[i],referral[i]);
         }
         
+        HashMap<String,Integer> totalMap = new HashMap<>();
         
         for(int i=0; i<seller.length; i++) {
-            String man = seller[i];
-            int money = amount[i] * 100; // 벌어들인 돈
-            int commission = money / 10; // 수수료
-            int earn = money - commission; // 벌어들인 돈 - 수수료
+            String curName = seller[i];
+            int money = amount[i] * 100; // 판매자가 판매한 금액
             
-            if(referralMap.containsKey(man)) {
-                sellerMap.put(man, sellerMap.get(man) + earn);
-                while(!referralMap.get(man).equals("-")) {
-                    man = referralMap.get(man);
-                    if (money == 0) break;
-                    else money /= 10;
-                    commission = money / 10;
-                    earn = money - commission;
-                    sellerMap.put(man, sellerMap.get(man) + earn);
-                }
+            //판매자 부터 부모 (다단계로 초대한) 한테 이동하면서 수수료를 판매자는 빼고, 부모는 더한다.
+            while(money > 0 && !curName.equals("-")) {
+                totalMap.put(curName, totalMap.getOrDefault(curName,0) + money - (money / 10));
+                curName = parent.get(curName);
+                money /= 10;
             }
         }
         
+        // System.out.println(totalMap);
         int[] answer = new int[enroll.length];
-        int index = 0;
-        for(String enrolls : enroll) {
-            answer[index++] = sellerMap.get(enrolls);
+        for(int i=0; i<enroll.length; i++) {
+            answer[i] = totalMap.getOrDefault(enroll[i],0);
         }
         return answer;
     }

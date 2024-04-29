94fd112607bd2d80d40e571dac3b61ed9639b47f
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-04-29 18:04:32 +0900

    [Silver II] Title: 차이를 최대로, Time: 152 ms, Memory: 15248 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int max_result = Integer.MIN_VALUE;
+    static int[] arr;
+    static int[] selected;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        selected = new int[N];
+        visited = new boolean[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0);
+        System.out.println(max_result);
+    }
+
+    private static void dfs(int count) {
+        if (count == N) {
+            int sum = 0;
+            for (int i = 0; i < N - 1; i++) {
+                sum += Math.abs(selected[i] - selected[i+1]);
+            }
+            max_result = Math.max(sum,max_result);
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                selected[count] = arr[i];
+                visited[i] = true;
+                dfs(count+1);
+                visited[i] = false;
+            }
+        }
+    }
+}

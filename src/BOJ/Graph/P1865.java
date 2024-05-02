c5124847fd59570b3f9e12d81a1b5390eeefc298
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-02 11:15:40 +0900

    Graph Algorithm

--- src/BOJ/Graph/P1865.java
+++ src/BOJ/Graph/P1865.java
@@ -1,2 +1,95 @@
-package BOJ.Graph;public class P1865 {
+package BOJ.Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Edge {
+    int end;
+    int cost;
+
+    public Edge(int end,int cost) {
+        this.end = end;
+        this.cost = cost;
+    }
+}
+
+/*
+* 테스트 케이스 tc
+*
+*웜홀은 시작 위치에서 도착 위치로 가는 하나의 경로인데,
+* 특이하게도 도착을 하게 되면 시작을 하였을 때보다 시간이 뒤로 가게 된다.
+* 웜홀 내에서는 시계가 거꾸로 간다고 생각하여도 좋다.
+* */
+public class P1865 {
+    static int N;
+    static int M;
+    static int W;
+    static ArrayList<ArrayList<Edge>> graph;
+    static int[] dist;
+    static int INF = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int tc = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < tc; i++) {
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+
+            dist = new int[N+1];
+            graph = new ArrayList<>(); //init graph
+
+            for (int j = 0; j <= N; j++) {
+                graph.add(new ArrayList<>());
+            }
+
+            for (int j = 0; j < M+W; j++) {
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+                int weight = Integer.parseInt(st.nextToken());
+
+                //Worm Hole X -> 양방향 그래프
+                if (i < M) {
+                    graph.get(start).add(new Edge(end,weight));
+                    graph.get(end).add(new Edge(start,weight));
+                } else {
+                    // Worm Hole -> 단방향 -> 가중치는 음수 -> 벨만포드?
+                    graph.get(start).add(new Edge(end,-weight));
+                }
+            }
+
+            sb.append(bellmanFord() ? "YES\n" : "NO\n");
+        }
+    }
+
+    private static boolean bellmanFord() {
+        Arrays.fill(dist,INF);
+        dist[1] = 0; // 시작점-> 0
+        boolean flag = false;
+
+        for (int i = 1; i < N; i++) {
+            flag = false;
+
+            for (int j = 1; j <= N; j++) {
+                for (Edge edge : graph.get(i)) {
+                    if (dist[edge.end] > dist[j] + edge.cost) {
+                        dist[edge.end] = dist[j] + edge.cost;
+                        flag = true;
+                    }
+                }
+            }
+        }
+
+        if (flag) {
+
+        }
+        return false;
+    }
 }
---
+++ src/BOJ/Graph/P1967.java
@@ -0,0 +1,66 @@
+package BOJ.Graph;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//가장 길게 늘어나는 경우 - > 최장거리를 구해야해 , DFS + 다익스트라
+public class P1967 {
+    private static class Node {
+        int dest, cost;
+
+        public Node(int dest, int cost) {
+            this.dest = dest;
+            this.cost = cost;
+        }
+    }
+
+    private static ArrayList<Node>[] adjList;
+    private static int answer;
+    private static boolean[] visited;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        adjList = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        for (int i = 0; i < n-1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            adjList[start].add(new Node(dest, cost));
+            adjList[dest].add(new Node(start, cost));
+        }
+
+        answer = Integer.MIN_VALUE;
+        visited = new boolean[n+1];
+
+        for (int i = 1; i < n+1; i++) {
+            Arrays.fill(visited,false);
+            dfs(i,0);
+        }
+        System.out.println(answer);
+    }
+    private static void dfs(int now, int cost) {
+        visited[now] = true;
+        answer = Math.max(answer,cost);
+
+        for (Node next : adjList[now]) {
+            int next_dest = next.dest;
+            int next_cost = next.cost;
+
+            if (!visited[next_dest]) {
+                dfs(next_dest, cost + next_cost);
+            }
+        }
+
+    }
+}

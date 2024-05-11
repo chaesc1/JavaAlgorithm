a30c1c89b8aac659e77f9b6dc579e9fb349cc609
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-12 02:22:45 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 125 ms, Memory: 28,536 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = 10;
+        int[] arr = new int[100];
+
+        StringTokenizer st;
+        for (int i = 1; i <= tc; i++) {
+            int dump = Integer.parseInt(br.readLine());
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 100; j++) {
+                arr[j] = Integer.parseInt(st.nextToken()); //배열 입력 받고 -> 정렬
+            }
+
+            for (int j = 0; j < dump; j++) {
+                Arrays.sort(arr);
+                arr[0]++;
+                arr[99]--;
+            }
+            Arrays.sort(arr);
+            System.out.println("#"+i+" "+(arr[99]-arr[0]));
+        }
+    }
+}

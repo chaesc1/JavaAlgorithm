c08c5c9d42b08afb7dcc599cdcc3a35bafd3d140
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-13 13:10:01 +0900

    [Gold V] Title: 두 용액, Time: 384 ms, Memory: 28500 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,53 @@
+// 두 용액
+
+// 투포인터 문제
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int start = 0;
+        int end = arr.length - 1;
+        int min = Integer.MAX_VALUE;
+
+        int[] result = new int[2];
+
+        // 배열 정렬
+        Arrays.sort(arr);
+
+        while (start < end) {
+            int sum = arr[start] + arr[end];
+
+            if (min > Math.abs(sum)) {
+                min = Math.abs(sum);
+
+                result[0] = arr[start];
+                result[1] = arr[end];
+
+                if (sum == 0) break;
+            }
+
+            if(sum < 0) start++;
+            else end--;
+
+        }
+
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+}

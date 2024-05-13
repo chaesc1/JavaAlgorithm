afb8396dcc37e20631bb0269777971fce8ec85e1
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-13 22:36:48 +0900

    [Gold IV] Title: 수 묶기, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //음수는 오름차순으로 우선순위 큐 생성
+        PriorityQueue<Integer> minus = new PriorityQueue<>();
+        //양수는 내림차순으로 우선순위 큐
+        PriorityQueue<Integer> plus = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > 0) {
+                plus.add(num);
+            } else {
+                minus.add(num);
+            }
+        }
+
+        int result = getSum(minus) + getSum(plus);
+        System.out.println(result);
+    }
+
+    private static int getSum(PriorityQueue<Integer> pq) {
+        Queue<Integer> queue = new LinkedList<>();
+        while (!pq.isEmpty()) {
+            int cur = pq.poll();
+
+            if (pq.isEmpty()) {
+                queue.add(cur);
+            } else {
+                int next = pq.poll();
+                if (cur == 1 || next == 1) {
+                    // 1은 곱하면 손해
+                    queue.add(cur);
+                    queue.add(next);
+                } else {
+                    queue.add(cur * next);
+                }
+            }
+        }
+        int result = 0;
+        while (!queue.isEmpty()) {
+            result += queue.poll();
+        }
+        return result;
+    }
+}

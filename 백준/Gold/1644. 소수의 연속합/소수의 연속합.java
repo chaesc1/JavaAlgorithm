de0a087293f01ae7595d32f60edeaac26beda4ac
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-13 21:38:16 +0900

    [Gold III] Title: 소수의 연속합, Time: 204 ms, Memory: 26996 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    static ArrayList<Integer> list;
+    static boolean[] prime;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        // 소수 전환
+        getPrime(N);
+
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+            if (sum > N) {
+                sum -= list.get(start);
+                start++;
+            } else if (list.size() == end) {
+                break;
+            }
+            else {
+                sum += list.get(end);
+                end++;
+            }
+            if (N == sum) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    //에라토스테네스의 체
+    private static void getPrime(int n) {
+        prime = new boolean[n+1];
+        list = new ArrayList<>();
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (!prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = true;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!prime[i]) list.add(i);
+        }
+    }
+}

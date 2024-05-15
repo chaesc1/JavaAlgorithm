c18356f3760c3290598863beb7dba927851ed9e7
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-15 19:37:16 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 480 ms, Memory: 40404 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        boolean isPrime = false;
+        boolean isPalin = false;
+
+        //소수 구하고
+
+        //palindrome 검사
+
+        while (true) {
+            isPrime = getPrime(N);
+            isPalin = checkPalin(Integer.toString(N));
+            if (isPrime && isPalin) break;
+            N++;
+        }
+
+        System.out.println(N);
+    }
+
+    private static boolean checkPalin(String num) {
+        int start = 0;
+        int end = num.length()-1;
+
+        while (start <= end) {
+            if (num.charAt(start) != num.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    private static boolean getPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}

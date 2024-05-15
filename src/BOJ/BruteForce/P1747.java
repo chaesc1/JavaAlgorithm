a56bbfe6369a041a238d4c67843e9647ae88ca97
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-15 19:38:51 +0900

    [Message] Add P1747 class for prime and palindrome problem
    This commit adds a new Java class, P1747, to the BOJ.BruteForce package. This class is designed to solve problems that involve identifying prime numbers and palindrome numerics. Inputs are received and processed to identify if they fulfill both criteria, with the results printed to the console.

---
+++ src/BOJ/BruteForce/P1747.java
@@ -0,0 +1,58 @@
+package BOJ.BruteForce;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P1747 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        boolean isPrime = false;
+        boolean isPalin = false;
+
+        if (N == 1 || N == 2) {
+            System.out.println(2);
+            return;
+        }
+        if (N % 2 == 0) {
+            N++;
+        }
+        //소수 구하고
+
+        //palindrome 검사
+
+        while (true) {
+            isPrime = getPrime(N);
+            isPalin = checkPalin(Integer.toString(N));
+            if (isPrime && isPalin) break;
+            N+=2;
+        }
+
+        System.out.println(N);
+    }
+
+    private static boolean checkPalin(String num) {
+        int start = 0;
+        int end = num.length()-1;
+
+        while (start <= end) {
+            if (num.charAt(start) != num.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    private static boolean getPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}

29de43c189879736fdc6d984638f894f91457a69
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-18 20:49:55 +0900

    백준 문제풀이

---
+++ src/SWexpertAcademy/D3/P19003.java
@@ -0,0 +1,12 @@
+package SWexpertAcademy.D3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P19003 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}

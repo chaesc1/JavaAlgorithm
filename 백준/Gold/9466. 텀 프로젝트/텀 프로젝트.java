867044fadc078e2fecb2316a0bc3d59aaa9a3688
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-05-27 23:13:12 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1184 ms, Memory: 304680 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    static boolean[] visited, isCheck;
+
+    static int n,res;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            n = Integer.parseInt(br.readLine());
+            res = 0;
+
+            arr = new int[n + 1];
+            visited = new boolean[n + 1];
+            isCheck = new boolean[n + 1];
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int j = 1; j <= n; j++) {
+//                if (isCheck[j]) continue;
+                dfs(j);
+            }
+            System.out.println(n-res);
+        }
+    }
+
+    private static void dfs(int index) {
+        if (isCheck[index]) return;
+        if (visited[index]) {
+            isCheck[index] = true;
+            res++;
+        }
+        visited[index] = true;
+        dfs(arr[index]);
+        isCheck[index] = true;
+        visited[index] = false;
+    }
+}

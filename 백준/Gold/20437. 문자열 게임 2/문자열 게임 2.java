d879185c37d5f2da3bded2793395de006af14bc9
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-06-18 15:57:05 +0900

    [Gold V] Title: 문자열 게임 2, Time: 520 ms, Memory: 21584 KB -BaekjoonHub

---
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine());
+        for (int t = 0; t < tc; t++) {
+            String w = br.readLine();
+            int k = Integer.parseInt(br.readLine());
+
+            if (k == 1) {
+                System.out.println("1 1");
+                continue;
+            }
+
+            // 주어진 문자열에서 알파벳 갯수를 카운트해본다
+            int[] alpha = new int[26];
+            for (int i = 0; i < w.length(); i++) {
+                alpha[w.charAt(i) - 'a']++;
+            }
+
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            // 해당 문자열을 처음 부터 탐색을 시작한다.
+            for (int i = 0; i < w.length(); i++) {
+                if (alpha[w.charAt(i) - 'a'] < k) {
+                    continue;
+                }
+
+                int count = 1;
+                for (int j = i + 1; j < w.length(); j++) {
+                    if (w.charAt(i) == w.charAt(j)) {
+                        count++;
+                    }
+
+                    if (count == k) {
+                        min = Math.min(min, (j - i) + 1);
+                        max = Math.max(max, (j - i) + 1);
+                        break;
+                    }
+                }
+            }
+
+            if (min == Integer.MAX_VALUE || max == Integer.MIN_VALUE) {
+                System.out.println("-1");
+            } else {
+                System.out.println(min + " " + max);
+            }
+        }
+
+    }
+}

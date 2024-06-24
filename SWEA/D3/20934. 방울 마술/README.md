233fea701b95b3cdbd4c20acb2dbb072dc2bd167
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-06-24 11:37:39 +0900

    [D3] Title: 방울 마술, Time: 103 ms, Memory: 25,232 KB -BaekjoonHub

---
+++ SWEA/D3/20934. 방울 마술/README.md
@@ -0,0 +1,15 @@
+# [D3] 방울 마술 - 20934 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AY9QTGqqcckDFAVF) 
+
+### 성능 요약
+
+메모리: 25,232 KB, 시간: 103 ms, 코드길이: 1,229 Bytes
+
+### 제출 일자
+
+2024-06-24 11:37
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/20934. 방울 마술/방울 마술.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= tc; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            int k = Integer.parseInt(st.nextToken());
+
+            int result = 0;
+            if (s.charAt(0) == 'o') {
+                result = 0;
+            } else if (s.charAt(1) == 'o') {
+                result = 1;
+            } else {
+                result = 2;
+            }
+
+            while (k > 0) {
+                switch (result) {
+                    case 0:
+                        result = 1;
+                        break;
+                    case 1:
+                        result = 0;
+                        break;
+                    case 2:
+                        result = 1;
+                        break;
+                }
+                k--;
+            }
+            System.out.println("#" + t + " " + result);
+        }
+
+
+    }
+}

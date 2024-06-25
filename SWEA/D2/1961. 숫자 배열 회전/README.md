81ab03826613197b5cf179d19812b0bdfbf46cd7
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-06-25 19:18:40 +0900

    [D2] Title: 숫자 배열 회전, Time: 118 ms, Memory: 22,912 KB -BaekjoonHub

---
+++ SWEA/D2/1961. 숫자 배열 회전/README.md
@@ -0,0 +1,15 @@
+# [D2] 숫자 배열 회전 - 1961 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Pq-OKAVYDFAUq) 
+
+### 성능 요약
+
+메모리: 22,912 KB, 시간: 118 ms, 코드길이: 1,879 Bytes
+
+### 제출 일자
+
+2024-06-25 19:18
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static int length;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= tc; t++) {
+            length = Integer.parseInt(br.readLine());
+            int[][] arr = new int[length][length];
+
+            for (int i = 0; i < length; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < length; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            // 90 도
+            int[][] rotate90 = rotate(arr);
+            // 180 도
+            int[][] rotate180 = rotate(rotate90);
+            // 270 도
+            int[][] rotate270 = rotate(rotate180);
+
+            System.out.println("#" + t);
+            for (int i = 0; i < length; i++) {
+                for (int j = 0; j < length; j++) {
+                    System.out.print(rotate90[i][j]);
+                }
+                System.out.print(" ");
+
+                for (int j = 0; j < length; j++) {
+                    System.out.print(rotate180[i][j]);
+                }
+                System.out.print(" ");
+
+                for (int j = 0; j < length; j++) {
+                    System.out.print(rotate270[i][j]);
+                }
+                System.out.println();
+            }
+        }
+
+    }
+
+    private static int[][] rotate(int[][] arr) {
+        int[][] result = new int[length][length];
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                result[j][length - 1 - i] = arr[i][j];
+            }
+        }
+        return result;
+    }
+}

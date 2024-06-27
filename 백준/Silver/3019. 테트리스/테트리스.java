4c862538f3bc4322190a5cd44d8adf851ce3fc7d
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-06-27 10:56:08 +0900

    [Silver I] Title: 테트리스, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/3019. 테트리스/테트리스.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int C = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken()) - 1;
+
+        int[] map = new int[C];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //도형의 밑변에 빈칸을 배열로
+        int[][][] tetris = {
+                {{0}, {0, 0, 0, 0}},
+                {{0, 0}},
+                {{0, 0, 1}, {1, 0}},
+                {{1, 0, 0}, {0, 1}},
+                {{0, 0, 0}, {1, 0}, {1, 0, 1}, {0, 1}},
+                {{0, 0, 0}, {0, 0}, {0, 1, 1}, {2, 0}},
+                {{0, 0, 0}, {0, 2}, {1, 1, 0}, {0, 0}}
+        };
+        int answer = 0;
+        for (int i = 0; i < tetris[P].length; i++) {
+            for (int j = 0; j < C - tetris[P][i].length + 1; j++) {
+                int diff = map[j] - tetris[P][i][0];
+                boolean isOk = true;
+                for (int k = 1; k < tetris[P][i].length; k++) {
+                    if (diff != map[j + k] - tetris[P][i][k]) {
+                        isOk = false;
+                        break;
+                    }
+                }
+                if (isOk) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}

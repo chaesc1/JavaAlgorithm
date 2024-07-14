60844f0ab1f7bad6b7e8c0fa0159e3bfe9758938
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-07-14 10:59:59 +0900

    [Silver III] Title: 파도반 수열, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static long[] arr = new long[101];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        pado();
+        for (int i = 0; i < tc; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+            System.out.println(arr[N]);
+        }
+
+    }
+
+    private static void pado() {
+        arr[1] = 1L;
+        arr[2] = 1L;
+        arr[3] = 1L;
+
+        for (int i = 4; i < 101; i++) {
+            arr[i] = arr[i-2] + arr[i-3];
+        }
+    }
+}

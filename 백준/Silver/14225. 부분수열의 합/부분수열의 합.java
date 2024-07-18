b8acbe0a35f7f2de160d4fd5c1280a2eafb09166
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-07-18 09:58:54 +0900

    [Silver I] Title: 부분수열의 합, Time: 128 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -1,39 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[] arr;
-    static boolean[] isUse;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-
-        arr = new int[N];
-        isUse = new boolean[100000 * 20 + 1];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        dfs(0, 0);
-        for (int i = 1; i < isUse.length; i++) {
-            if (!isUse[i]) {
-                System.out.println(i);
+        Arrays.sort(arr);
+        int minSum = 1;
+        for (int i = 0; i < n; i++) {
+            if (minSum < arr[i]) {
                 break;
             }
+            minSum += arr[i];
         }
-    }
-
-    private static void dfs(int sum, int depth) {
-        if (depth == N) {
-            isUse[sum] = true;
-            return;
-        }
-        dfs(sum + arr[depth], depth + 1);
-        dfs(sum, depth + 1);
+        System.out.println(minSum);
     }
 }

1a646262799ad85840b1eb1a091f7cd290b2fefa
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-07-22 00:23:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,85 +1,75 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    static int[][] map;
-    static boolean[][] visited;
-
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
     static int N;
+    static int[][] map;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
     static int count;
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+
         map = new int[N][N];
         visited = new boolean[N][N];
+
         for (int i = 0; i < N; i++) {
-            String num = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = num.charAt(j) - '0';
+                map[i][j] = line.charAt(j) - '0';
             }
         }
 
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visited[i][j]){
-                    count=0;
-//                    dfs(i,j);
-                    bfs(i,j);
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    count = 0;
+                    bfs(i, j);
                     list.add(count);
                 }
             }
         }
-        System.out.println(list.size());
+
         Collections.sort(list);
-        for(int num : list){
-            System.out.println(num);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(list.size()).append("\n");
+        for (Integer i : list) {
+            sb.append(i).append("\n");
         }
+
+        System.out.println(sb);
     }
 
-    private static void dfs(int x, int y) {
+    private static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
         count++;
         visited[x][y] = true;
+        q.offer(new int[]{x,y});
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int curX = cur[0];
+            int curY = cur[1];
 
-            if(0<=nx && nx<N && 0<=ny && ny<N){
-                if (!visited[nx][ny] && map[nx][ny] == 1){
-                    visited[nx][ny] = true;
-                    dfs(nx,ny);
-                }
-            }
-        }
-    }
-    private static void bfs(int x,int y){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {x,y});
-        count++;
-        visited[x][y] = true;
-        while (!queue.isEmpty()){
-            int curX = queue.peek()[0];
-            int cutY = queue.peek()[1];
-            queue.poll();
-
+            // 4방 탐색
             for (int i = 0; i < 4; i++) {
                 int nx = curX + dx[i];
-                int ny = cutY + dy[i];
+                int ny = curY + dy[i];
 
-                if(0<=nx && nx < N && 0 <= ny && ny < N){
-                    //방문하지 않고 영역이면?
-                    if(!visited[nx][ny] && map[nx][ny] == 1){
-                        //방문표시
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    if (!visited[nx][ny] && map[nx][ny] == 1) {
                         visited[nx][ny] = true;
-                        queue.offer(new int[] {nx,ny});
+                        q.offer(new int[] {nx,ny});
                         count++;
                     }
                 }

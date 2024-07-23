e4f7c33f3e3419065ba0274417cd6c3016182a19
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-07-23 22:38:47 +0900

    [Silver II] Title: 스택 수열, Time: 364 ms, Memory: 26820 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,40 +1,39 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args)throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int A[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            A[i] = sc.nextInt();
-        }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        boolean isOk = true;
+        StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
-        int num = 1;
-        boolean result = true;
-        StringBuffer bf = new StringBuffer();
-        for (int i = 0; i < A.length; i++) {
-            int temp = A[i];
-            if(temp >= num){
-                while (temp >= num){
-                    stack.push(num++);
-                    bf.append("+\n");
-                }
+
+        int tmp = 1;
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            for (; tmp <= num; tmp++) {
+                stack.push(tmp);
+                sb.append("+").append("\n");
+            }
+
+            if (stack.peek() == num) {
                 stack.pop();
-                bf.append("-\n");
-            }else{
-                int temp2 = stack.pop();
-                if(temp2 > temp){
-                    System.out.println("NO");
-                    result = false;
-                    break;
-                }
-                else{
-                    bf.append("-\n");
-                }
+                sb.append("-").append("\n");
+            } else {
+                isOk = false;
             }
         }
-        if(result) System.out.println(bf.toString());
+
+        if(isOk) {
+            System.out.println(sb);
+        } else {
+            System.out.println("NO");
+        }
+
     }
 }

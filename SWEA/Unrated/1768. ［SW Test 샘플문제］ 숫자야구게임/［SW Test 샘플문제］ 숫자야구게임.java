6697b9b3e874847e70e0e16a4024a7b40fe9a469
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-07-26 02:27:46 +0900

    [Unrated] Title: [SW Test 샘플문제] 숫자야구게임, Time: 196 ms, Memory: 42,120 KB -BaekjoonHub

--- SWEA/Unrated/1768. ［SW Test 샘플문제］ 숫자야구게임/［SW Test 샘플문제］ 숫자야구게임.java
+++ SWEA/Unrated/1768. ［SW Test 샘플문제］ 숫자야구게임/［SW Test 샘플문제］ 숫자야구게임.java
@@ -9,12 +9,14 @@ class UserSolution {
         num = new int[5040];
         isPossible = new boolean[9877];
 
+        // 가능한 모든 경우의 수를 저장
         for (int i = 123; i <= 9876; i++) {
             int n1 = i / 1000;
-            int n2 = i % 1000 / 100;
-            int n3 = i % 100 / 10;
+            int n2 = (i / 100) % 10;
+            int n3 = (i / 10) % 10;
             int n4 = i % 10;
 
+            // 각 자리의 숫자가 중복되지 않는 경우만 저장
             if (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4) {
                 num[index++] = i;
                 isPossible[i] = true;
@@ -23,34 +25,30 @@ class UserSolution {
     }
 
     public void doUserImplementation(int guess[]) {
-        // Implement a user's implementation function
-        //
-        // The array of guess[] is a return array that
-        // is your guess for what digits[] would be.
         init();
         int answer = 0;
-        int strike = 0;
-        int ball = 0;
-        int target = 0;
-        int result = 0;
+        int strike, ball, target, result;
         int[] ballCount;
 
         while (true) {
+            // 가능한 숫자 중 하나를 선택
             for (int i = 0; i < index; i++) {
                 if (isPossible[num[i]]) {
                     answer = num[i];
                     guess[0] = answer / 1000;
-                    guess[1] = answer % 1000 / 100;
-                    guess[2] = (answer % 100) / 10;
+                    guess[1] = (answer / 100) % 10;
+                    guess[2] = (answer / 10) % 10;
                     guess[3] = answer % 10;
                     break;
                 }
             }
 
+            // 결과를 쿼리
             Solution.Result results = Solution.query(guess);
-            if (results.strike == 4) break;
+            if (results.strike == 4) break;  // 정답을 맞춘 경우 종료
             else isPossible[answer] = false;
 
+            // 모든 가능한 숫자에 대해 가지치기
             for (int i = 0; i < index; i++) {
                 if (isPossible[num[i]]) {
                     result = answer;
@@ -60,18 +58,24 @@ class UserSolution {
                     ballCount = new int[10];
 
                     for (int j = 0; j < 4; j++) {
-                        if (result % 10 == target % 10) strike++;
-                        else {
-                            ballCount[result % 10]++;
-                            ballCount[target % 10]++;
+                        int resultDigit = result % 10;
+                        int targetDigit = target % 10;
+                        if (resultDigit == targetDigit) {
+                            strike++;
+                        } else {
+                            ballCount[resultDigit]++;
+                            ballCount[targetDigit]++;
                         }
                         result /= 10;
                         target /= 10;
                     }
+
+                    // 볼 개수 계산
                     for (int j = 0; j < 10; j++) {
                         if (ballCount[j] == 2) ball++;
                     }
 
+                    // 가지치기 조건
                     if (strike != results.strike || ball != results.ball) {
                         isPossible[num[i]] = false;
                     }

d8b8e4d42f0a7b97b612f4eb11bfea8f627f81a1
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-07-27 20:05:09 +0900

    Add basic structure for P20207 implementation
    Created a skeleton for the P20207 class including package declaration and basic input handling setup. This commit sets up the groundwork for further implementation of the problem solution.

---
+++ src/BOJ/Implement/P20207.java
@@ -0,0 +1,12 @@
+package BOJ.Implement;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P20207 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}

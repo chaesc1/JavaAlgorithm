077ccaa2e61896d19b8f5f4d1dc80b9d80ed8662
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-07-29 17:33:57 +0900

    [Silver II] Title: 쇠막대기, Time: 184 ms, Memory: 16352 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        Stack<Character> stack = new Stack<Character>();
+
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == '(') {
+                stack.push('(');
+                continue;
+            }
+            if(input.charAt(i) == ')') {
+                stack.pop();
+
+                if(input.charAt(i-1) == '(') {
+                    result += stack.size();
+                } else {
+                    result ++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}

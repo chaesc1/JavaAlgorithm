2051b2be7a66b2df4e0477d0746189708514a193
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-06 20:31:58 +0900

    [level 2] Title: 배달, Time: 3.48 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,46 +1,44 @@
 import java.util.*;
 
 class Solution {
-    private static class Node {
-        int dest, cost;
-        public Node(int dest, int cost) {
-            this.dest = dest;
+    static class Node {
+        int end;
+        int cost;
+        
+        public Node(int end,int cost) {
+            this.end = end;
             this.cost = cost;
         }
     }
+    
     public int solution(int N, int[][] road, int K) {
-        // 인접 리스트 저장할 배열 초기화
         ArrayList<Node>[] adjList = new ArrayList[N+1];
+        
         for(int i=1; i<=N; i++) {
             adjList[i] = new ArrayList<>();
         }
         
-        //road 정보를 인접리스트에 저장 - 무방향 그래프
         for(int[] edge : road) {
             adjList[edge[0]].add(new Node(edge[1],edge[2]));
             adjList[edge[1]].add(new Node(edge[0],edge[2]));
         }
         
         int[] dist = new int[N+1];
-        
         Arrays.fill(dist,Integer.MAX_VALUE);
         
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2) -> Integer.compare(o1.cost , o2.cost));
-        
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2) -> Integer.compare(o1.cost ,o2.cost));
         pq.add(new Node(1,0));
-        dist[1] = 0; // 시작점은 dist = 0 으로
+        dist[1] = 0; // 시작점은 0으로 초기화
         
         while(!pq.isEmpty()) {
             Node now = pq.poll();
             
-            if(dist[now.dest] < now.cost) {
-                continue;
-            }
+            if(now.cost > dist[now.end]) continue;
             
-            for(Node next : adjList[now.dest]) {
-                if(dist[next.dest] > now.cost + next.cost) {
-                    dist[next.dest] = now.cost + next.cost;
-                    pq.add(new Node (next.dest,dist[next.dest]));
+            for(Node next : adjList[now.end]) {
+                if(now.cost + next.cost < dist[next.end]) {
+                    dist[next.end] = now.cost + next.cost;
+                    pq.add(new Node(next.end,dist[next.end]));
                 }
             }
         }

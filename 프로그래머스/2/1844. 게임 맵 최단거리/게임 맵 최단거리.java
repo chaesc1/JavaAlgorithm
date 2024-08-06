59f5900d3b92600b5800f14e32485545fa7839f0
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-06 20:09:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.32 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,57 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     
-    private static class Node {
-        int r,c;
+    static class Node {
+        int x;
+        int y;
         
-        public Node(int r,int c) {
-            this.r = r;
-            this.c = c;
+        public Node(int x,int y) {
+            this.x = x;
+            this.y = y;
         }
     }
+
+    static int N;
+    static int M;
     public int solution(int[][] maps) {
         int answer = 0;
         
-        int N = maps.length;
-        int M = maps[0].length;
+        N = maps.length;
+        M = maps[0].length;
         
         int[][] dist = new int[N][M];
         
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-        queue.addLast(new Node(0,0));
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0,0));
         dist[0][0] = 1;
         
-        while(!queue.isEmpty()) {
-            Node now = queue.pollFirst();
+        
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            
             for(int i=0; i<4; i++) {
-                int nr = now.r + dx[i];
-                int nc = now.c + dy[i];
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
                 
-                // 범위 체크
-                if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+                if (!isValid(nx,ny)) continue;
                 
-                if(maps[nr][nc] == 0) continue;
+                if(maps[nx][ny] == 0) continue;
                 
-                //처음 방문한 위치는 큐에 저장하고 거리를 갱신
-                if(dist[nr][nc] == 0) {
-                    queue.addLast(new Node(nr,nc));
-                    dist[nr][nc] = dist[now.r][now.c] + 1; 
+                // 처음 방문한 위치는 큐에 저장하고 거리 갱신
+                if (dist[nx][ny] == 0) {
+                    q.add(new Node(nx,ny));
+                    dist[nx][ny] = dist[cur.x][cur.y] + 1;
                 }
             }
         }
+        
         return dist[N-1][M-1] == 0 ? -1 : dist[N-1][M-1];
+    }
+    
+    private static boolean isValid(int x,int y) {
+        return x>=0 && x<N && y>=0 && y<M;
     }
 }

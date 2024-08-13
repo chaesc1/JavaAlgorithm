82843de85dfb964976c19a820b6e2cd8f059927f
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-13 15:35:33 +0900

    [D2] Title: 달팽이 숫자, Time: 103 ms, Memory: 18,624 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,76 +1,55 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.FileInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1,0};
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < tc; i++) {
-            int input = Integer.parseInt(br.readLine());
-            int[][] map = new int[input][input];
-            int count = 1;
-            //시작좌표
-            int x=0;
-            int y=0;
-            int d = 0;
+public class Solution {
+    private static final int[] DX = {0, 1, 0, -1};
+    private static final int[] DY = {1, 0, -1, 0};
 
-            while (count <= input * input){
-                map[x][y] = count++;
-                int nx = x + dx[d];
-                int ny = y + dy[d];
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int tc = Integer.parseInt(br.readLine());
+            StringBuilder output = new StringBuilder();
 
-                if(nx < 0 || nx >= input || ny < 0 || ny >= input || map[nx][ny] != 0){
-                    d = (d+1) % 4;
-                    nx = x + dx[d];
-                    ny = y + dy[d];
-                }
-                x = nx;
-                y = ny;
+            for (int i = 0; i < tc; i++) {
+                int size = Integer.parseInt(br.readLine());
+                int[] map = new int[size * size];
+                fillSnailMap(map, size);
+                appendMap(output, map, size, i + 1);
             }
-            System.out.println("#"+(i+1));
-            for (int j = 0; j < input; j++) {
-                for (int k = 0; k < input; k++) {
-                    System.out.print(map[j][k]+" ");
-                }
-                System.out.println();
+
+            System.out.print(output.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fillSnailMap(int[] map, int size) {
+        int count = 1;
+        int x = 0, y = 0, d = 0;
+
+        while (count <= size * size) {
+            map[x * size + y] = count++;
+            int nx = x + DX[d];
+            int ny = y + DY[d];
+
+            if (nx < 0 || nx >= size || ny < 0 || ny >= size || map[nx * size + ny] != 0) {
+                d = (d + 1) % 4;
+                nx = x + DX[d];
+                ny = y + DY[d];
             }
+            x = nx;
+            y = ny;
+        }
+    }
+
+    private static void appendMap(StringBuilder output, int[] map, int size, int caseNumber) {
+        output.append("#").append(caseNumber).append("\n");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                output.append(map[i * size + j]).append(" ");
+            }
+            output.append("\n");
         }
     }
 }

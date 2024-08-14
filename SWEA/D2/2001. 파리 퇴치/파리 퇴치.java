1ece0905266ccf2b2243f5c71de7929c2319037d
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-14 17:31:36 +0900

    [D2] Title: 파리 퇴치, Time: 127 ms, Memory: 18,932 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,44 +1,57 @@
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
+/***
+ *
+ */
+ 
 public class Solution {
-    static int n, m;
-    static int[][] board;
-    static int[][] sum;
-    static StringBuilder sb = new StringBuilder();
-
+    static int cnt;
+    static int T, N, M, maxTotal;
+    static int[][] graph;
+    static boolean[][] visited;
+    // 이차원 배열로 탐색한다 + cnt로 시작점 (x, y)를 넘겨줘야 한다. -> 최대값 계속 갱신해주기!
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
-        int testcase = Integer.parseInt(br.readLine());
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        T = Integer.parseInt(bf.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            maxTotal = 0;
+            graph = new int[N][N];
+            visited = new boolean[N][N];
 
-        for (int t = 1; t <= testcase; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            board = new int[n][n];
-            sum = new int[n][n];
-
-            for (int i = 0; i < n; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
-                    board[i][j] = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(bf.readLine());
+                for (int j = 0; j < N; j++) {
+                    graph[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-
-            int max = 0;
-            for (int i = 0; i <= n - m; i++) {
-                for (int j = 0; j <= n - m; j++) {
-                    int sum = 0;
-                    for (int k = i; k < i + m; k++) {
-                        for (int l = j; l < j + m; l++) {
-                            sum += board[k][l];
-                        }
-                    }
-                    max = Math.max(max, sum);
-                }
-            }
-            sb.append("#").append(t).append(" ").append(max).append("\n");
+            check(0, 0);
+            sb.append("#").append(tc).append(" ").append(maxTotal).append("\n");
         }
         System.out.println(sb);
+    }
+ 
+    private static void check(int x, int y) {
+        int total = 0;
+        if (visited[x][y]) return;
+        visited[x][y] = true;
+        for (int i = x; i < x + M; i++) {
+            for (int j = y; j < y + M; j++) {
+
+                if (i >= N || j >= N)
+                    return;
+                total += graph[i][j];
+            }
+        }
+        if (maxTotal < total)
+            maxTotal = total;
+        check(x + 1, y);
+        check(x, y + 1);
+ 
     }
 }

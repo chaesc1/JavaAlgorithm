6c5121497eaa85bdcdaad451e4abd49c29b7d9da
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-14 13:49:40 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -3,24 +3,25 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n,m;
+    static int n, m;
     static int[] arr;
     static boolean[] isSelected;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         arr = new int[m];
-        isSelected = new boolean[n+1];
+        isSelected = new boolean[n + 1];
 
-        backTracking(0,0);
+        backTracking(0, 1);
         System.out.println(sb.toString());
     }
 
-    private static void backTracking(int depth,int num) {
+    private static void backTracking(int depth, int num) {
         if (depth == m) {
             for (int i = 0; i < m; i++) {
                 sb.append(arr[i]).append(" ");
@@ -29,13 +30,9 @@ public class Main {
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (!isSelected[i] && num < i){
-                isSelected[i] = true;
-                arr[depth] = i;
-                backTracking(depth+1,i);
-                isSelected[i] = false;
-            }
+        for (int i = num; i <= n; i++) {
+            arr[depth] = i;
+            backTracking(depth + 1, i+1);
         }
     }
 }

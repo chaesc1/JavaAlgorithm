187b815ea31846f3271925becc514639a9c27c2e
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-16 17:07:32 +0900

    [Silver II] Title: DNA 비밀번호, Time: 256 ms, Memory: 22420 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,100 +1,93 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int s,p,ans;
-	static char[] dna;
-	static int minA,minC,minG,minT;//최소로 들어있어야할 값
-	static int cntA,cntC,cntG,cntT;//실제로 부분문자열에 카운트 되어야 할 값
-	
-	private static void check() {
-		if(minA <= cntA && minC <= cntC && minG <= cntG && minT <= cntT) {
-			ans++;
-		}
-	}
-	
-	private static void solve() {
-		for(int i=0; i<p; i++) {
-			switch (dna[i]) {
-			case 'A':
-				cntA++;
-				break;
-			case 'C':
-				cntC++;
-				break;
-			case 'G':
-				cntG++;
-				break;
-			case 'T':
-				cntT++;
-				break;
-				
-			}
-		}
-		check();//최소 문자수 체크하고
-		//슬라이딩 윈도우 수행
-		for(int i=p; i<s; i++) {
-			switch (dna[i-p]) {
-			case 'A':
-				cntA--;
-				break;
-			case 'C':
-				cntC--;
-				break;
-			case 'G':
-				cntG--;
-				break;
-			case 'T':
-				cntT--;
-				break;
-				
-			}
-			switch (dna[i]) {
-			case 'A':
-				cntA++;
-				break;
-			case 'C':
-				cntC++;
-				break;
-			case 'G':
-				cntG++;
-				break;
-			case 'T':
-				cntT++;
-				break;
-				
-			}
-			
-			check();
-		}
-	}
-	
-	
- 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		s = Integer.parseInt(st.nextToken());
-		p = Integer.parseInt(st.nextToken());
-		
-		
-		//DNA 입력 
-		dna = new char[s];
-		dna = bf.readLine().toCharArray();
-		
-		//만족해야할 최소 갯수
-		st = new StringTokenizer(bf.readLine());
-		minA = Integer.parseInt(st.nextToken());
-		minC = Integer.parseInt(st.nextToken());
-		minG = Integer.parseInt(st.nextToken());
-		minT = Integer.parseInt(st.nextToken());
-		
-		solve();
-		System.out.println(ans);
- 	}
+    static int S, P;
+    static int minA, minC, minG, minT;
+    static int cntA, cntC, cntG, cntT;
+    static char[] dna;
+
+    static int answer;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        S = Integer.parseInt(st.nextToken()); // 문자열 길이
+        P = Integer.parseInt(st.nextToken()); // 부분 문자열의 길이
+
+        dna = br.readLine().toCharArray();
+
+        st = new StringTokenizer(br.readLine());
+        minA = Integer.parseInt(st.nextToken());
+        minC = Integer.parseInt(st.nextToken());
+        minG = Integer.parseInt(st.nextToken());
+        minT = Integer.parseInt(st.nextToken());
+
+        solve();
+        System.out.println(answer);
+        br.close();
+    }
+
+    private static void solve() {
+        for (int i = 0; i < P; i++) {
+            switch (dna[i]) {
+                case 'A':
+                    cntA++;
+                    break;
+                case 'C':
+                    cntC++;
+                    break;
+                case 'G':
+                    cntG++;
+                    break;
+                case 'T':
+                    cntT++;
+                    break;
+            }
+        }
+
+        // min 값 이상인지 체크한번하고
+        isValid();
+        // 한칸씩 슬라이드해서 다시 탐색
+        // 이전 값은 빼줘
+        for (int i = P; i < S; i++) {
+            switch (dna[i-P]) {
+                case 'A':
+                    cntA--;
+                    break;
+                case 'C':
+                    cntC--;
+                    break;
+                case 'G':
+                    cntG--;
+                    break;
+                case 'T':
+                    cntT--;
+                    break;
+            }
+
+            switch (dna[i]) {
+                case 'A':
+                    cntA++;
+                    break;
+                case 'C':
+                    cntC++;
+                    break;
+                case 'G':
+                    cntG++;
+                    break;
+                case 'T':
+                    cntT++;
+                    break;
+            }
+            isValid();
+        }
+    }
+
+    private static void isValid() {
+        if (cntA >= minA && cntC >= minC && cntG >= minG && cntT >= minT) {
+            answer++;
+        }
+    }
 }
- 

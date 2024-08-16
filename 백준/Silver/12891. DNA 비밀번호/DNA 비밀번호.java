ffacc725622979865614d647d86ba7907f450e56
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-16 17:09:26 +0900

    [Silver II] Title: DNA 비밀번호, Time: 252 ms, Memory: 21916 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,13 +2,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int S, P;
-    static int minA, minC, minG, minT;
-    static int cntA, cntC, cntG, cntT;
-    static char[] dna;
 
-    static int answer;
+public class Main {
+    private static int S, P;
+    private static int minA, minC, minG, minT;
+    private static int cntA, cntC, cntG, cntT;
+    private static char[] dna;
+
+    private static int answer;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,62 +30,62 @@ public class Main {
         br.close();
     }
 
+    // 초기 윈도우 및 슬라이딩 윈도우 알고리즘을 수행하는 함수
     private static void solve() {
+        // 초기 윈도우 처리
         for (int i = 0; i < P; i++) {
-            switch (dna[i]) {
-                case 'A':
-                    cntA++;
-                    break;
-                case 'C':
-                    cntC++;
-                    break;
-                case 'G':
-                    cntG++;
-                    break;
-                case 'T':
-                    cntT++;
-                    break;
-            }
+            add(dna[i]);
         }
 
-        // min 값 이상인지 체크한번하고
+        // 초기 윈도우 검증
         isValid();
-        // 한칸씩 슬라이드해서 다시 탐색
-        // 이전 값은 빼줘
-        for (int i = P; i < S; i++) {
-            switch (dna[i-P]) {
-                case 'A':
-                    cntA--;
-                    break;
-                case 'C':
-                    cntC--;
-                    break;
-                case 'G':
-                    cntG--;
-                    break;
-                case 'T':
-                    cntT--;
-                    break;
-            }
 
-            switch (dna[i]) {
-                case 'A':
-                    cntA++;
-                    break;
-                case 'C':
-                    cntC++;
-                    break;
-                case 'G':
-                    cntG++;
-                    break;
-                case 'T':
-                    cntT++;
-                    break;
-            }
+        // 슬라이딩 윈도우 처리
+        for (int i = P; i < S; i++) {
+            // 이전 윈도우의 첫 문자를 제거하고 새로운 문자를 추가
+            remove(dna[i - P]);
+            add(dna[i]);
             isValid();
         }
     }
 
+    // 특정 문자를 추가하여 카운트를 증가시키는 함수
+    private static void add(char c) {
+        switch (c) {
+            case 'A':
+                cntA++;
+                break;
+            case 'C':
+                cntC++;
+                break;
+            case 'G':
+                cntG++;
+                break;
+            case 'T':
+                cntT++;
+                break;
+        }
+    }
+
+    // 특정 문자를 제거하여 카운트를 감소시키는 함수
+    private static void remove(char c) {
+        switch (c) {
+            case 'A':
+                cntA--;
+                break;
+            case 'C':
+                cntC--;
+                break;
+            case 'G':
+                cntG--;
+                break;
+            case 'T':
+                cntT--;
+                break;
+        }
+    }
+
+    // 현재 윈도우가 유효한지 체크하는 함수
     private static void isValid() {
         if (cntA >= minA && cntC >= minC && cntG >= minG && cntT >= minT) {
             answer++;

580fd114c5197d0acd32f0e9ca479f25421de12f
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-18 00:47:29 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 114 ms, Memory: 20,000 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/README.md
@@ -0,0 +1,15 @@
+# [D2] 새로운 불면증 치료법 - 1288 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV18_yw6I9MCFAZN) 
+
+### 성능 요약
+
+메모리: 20,000 KB, 시간: 114 ms, 코드길이: 1,036 Bytes
+
+### 제출 일자
+
+2024-08-18 00:47
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= TC; tc++) {
+            int num = Integer.parseInt(br.readLine());
+            int count = 1; // 곱할 수
+
+            boolean[] cap = new boolean[10];
+            int capCount = 0;
+
+            while (capCount < 10) {
+                int multi = num * count;
+                String str = String.valueOf(multi);
+
+                for (int i = 0; i < str.length(); i++) {
+                    int digit = Character.digit(str.charAt(i), 10);
+
+                    if (!cap[digit]) {
+                        cap[digit] = true;
+                        capCount++;
+                    }
+                }
+                count++;
+            }
+            System.out.println("#" + tc + " " + num * (count - 1));
+        }
+    }
+}

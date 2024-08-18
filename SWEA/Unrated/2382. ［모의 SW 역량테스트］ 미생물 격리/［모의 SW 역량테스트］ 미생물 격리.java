04f3eef51071147e5f8de980f361404a56881315
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-19 02:04:35 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 1,058 ms, Memory: 108,980 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -4,15 +4,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
-/**
- * 미생물 군집이 이동 후 약품이 칠해진 셀에 도착하면 군집 내 미생물의 절반이 죽고, 이동방향이 반대로 바뀐다.
- * <p>
- * 미생물 수가 홀수인 경우 반으로 나누어 떨어지지 않으므로, 다음과 같이 정의한다.
- * <p>
- * 살아남은 미생물 수 = 원래 미생물 수를 2로 나눈 후 소수점 이하를 버림 한 값
- */
 public class Solution {
-    static class Cell implements Comparator<Cell> {
+    static class Cell{
         int x;
         int y;
         int count; // 미생물수
@@ -25,10 +18,6 @@ public class Solution {
             this.dir = dir;
         }
 
-        @Override
-        public int compare(Cell o1, Cell o2) {
-            return o2.count - o1.count;
-        }
     }
 
     static class TempCell {
@@ -90,7 +79,7 @@ public class Solution {
                     // 약품이면
                     if (isChem(nx, ny)) {
                         cell.count /= 2; // 미생물 수 절반
-                        cell.dir ^= 1;
+                        cell.dir = changeDir(curDir);
                     }
 
                     if (tempMap[nx][ny] == null) {
@@ -117,6 +106,16 @@ public class Solution {
 
             System.out.println("#" + tc + " " + cells.stream().mapToInt(cell -> cell.count).sum());
         }
+    }
+
+    // 0 1 2 3 상하좌우
+    private static int changeDir(int dir) {
+        if (dir % 2 == 0) {
+            dir += 1;
+        } else {
+            dir -= 1;
+        }
+        return dir;
     }
 
     private static boolean isChem(int nx, int ny) {

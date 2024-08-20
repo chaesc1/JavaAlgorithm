f7074b0a58b2e425cad8c8ba9837f59936929fab
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-20 14:06:23 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 161 ms, Memory: 18,260 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -1,41 +1,39 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int N, B, answer;
+    static int N, B;
     static int[] arr;
-
+    static int result;
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
         StringTokenizer st;
-        for (int t = 1; t <= tc; t++) {
+        int testcase = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= testcase; tc++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             B = Integer.parseInt(st.nextToken());
-
             arr = new int[N];
+
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
-            answer = Integer.MAX_VALUE;
-            dfs(0, 0);
-            System.out.println("#" + t + " " + (answer - B));
+            result = Integer.MAX_VALUE;
+            dfs(0,0);
+            System.out.println("#"+tc+" "+(result-B));
         }
     }
 
-    private static void dfs(int start, int sum) {
+    private static void dfs(int idx, int sum) {
         if (sum >= B) {
-            answer = Math.min(answer, sum);
+            result = Math.min(result, sum);
             return;
         }
 
-        for (int i = start; i < N; i++) {
-            if (sum + arr[i] < answer) {
-                dfs(i+1, sum+arr[i]);
+        for (int i = idx; i < N; i++) {
+            if (arr[i] + sum < result) {
+                dfs(i+1, arr[i] + sum);
             }
         }
     }

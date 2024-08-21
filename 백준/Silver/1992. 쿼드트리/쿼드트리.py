ecec045570c545b24568e3bccb989a7859de1c4a
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-21 14:07:59 +0900

    [Silver I] Title: 쿼드트리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -0,0 +1,28 @@
+n = int(input())
+map = [list(map(int,input())) for _ in range(n)]
+# print(map)
+
+def quad(x,y,n):
+    flag = map[x][y]
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if flag != map[i][j]:
+                flag = -1
+                break
+    
+    if flag == -1:
+        print('(',end='')
+        n = n // 2 
+
+        quad(x,y,n)
+        quad(x,y+n,n)
+        quad(x+n,y,n)
+        quad(x+n,y+n,n)
+
+        print(')',end='')
+    elif flag == 1:
+        print(1,end='')
+    else:
+        print(0,end='')
+
+quad(0,0,n)

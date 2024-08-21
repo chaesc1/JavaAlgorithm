7e8d56f02a6d3f89a21da963c2b9f7351db3fe1f
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-08-21 15:45:25 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 189 ms, Memory: 42,352 KB -BaekjoonHub

---
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/README.md
@@ -0,0 +1,15 @@
+# [D5] 현주가 좋아하는 제곱근 놀이 - 6782 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWgqsAlKr9sDFAW0) 
+
+### 성능 요약
+
+메모리: 42,352 KB, 시간: 189 ms, 코드길이: 1,122 Bytes
+
+### 제출 일자
+
+2024-08-21 15:45
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * - 2 이상의 어떤 정수 N이 있다.
+ * - N을 N+1로 바꿀 수 있다.
+ *
+ * - 이 정수일 때, N을 제곱근으로 바꿀 수 있다.
+ *
+ * 게임의 목표는 N을 2로 만드는 것이다.
+ * N을 2로 만들기 위해 조작해야 하는 횟수의 최솟값을 구하는 프로그램을 작성하라.
+ */
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= TC; tc++) {
+            long N = Long.parseLong(br.readLine());
+            long cnt = 0;
+
+            while (N > 2) {
+                long sqrt = (long) Math.sqrt(N);
+
+                if (sqrt * sqrt == N) {
+                    N = sqrt;
+                    cnt++;
+                } else {
+                    long nextPerfectSquare = (sqrt + 1) * (sqrt + 1);
+                    cnt += (nextPerfectSquare - N);
+                    N = nextPerfectSquare;
+                }
+            }
+
+            System.out.println("#" + tc + " " + cnt);
+        }
+    }
+}

748669aaa662c1e4aaaed9916a6000a665ecce68
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-09-03 16:49:14 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 207 ms, Memory: 20,372 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -1,79 +1,85 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Solution {
+    static int N, M, C;
+    static int[][] map;
+    static int[][] maxProfit;
 
-    private static int N, M, C;
-    private static int[][] honey;
-    private static int[][] maxHoneyProfit;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int t = 1; t <= T; t++) {
-            N = sc.nextInt();
-            M = sc.nextInt();
-            C = sc.nextInt();
-            honey = new int[N][N];
-            maxHoneyProfit = new int[N][N];
+        int TC = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= TC; tc++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
+
+            map = new int[N][N];
+            maxProfit = new int[N][N];
 
             for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    honey[i][j] = sc.nextInt();
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            // 각 벌통 선택의 최대 수익을 미리 계산
-            precomputeMaxProfit();
+            // 각 벌통 선택의 최대 수익 계산
+            preComputeMaxProfit();
             int maxTotalProfit = getMaxTotalProfit();
-            System.out.printf("#%d %d%n", t, maxTotalProfit);
+            sb.append("#").append(tc).append(" ").append(maxTotalProfit).append("\n");
         }
-        sc.close();
+        System.out.println(sb.toString());
     }
 
-    private static void precomputeMaxProfit() {
+    private static int getMaxTotalProfit() {
+        int maxProfits = 0;
+
+        // 선택할 수 있는 두 벌통 조합 중 최대 수익을 찾음
+        for (int i1 = 0; i1 < N; i1++) {
+            for (int j1 = 0; j1 <= N - M; j1++) {
+                for (int i2 = i1; i2 < N; i2++) {
+                    for (int j2 = (i1 == i2 ? j1 + M : 0); j2 <= N - M; j2++) {
+                        int profit1 = maxProfit[i1][j1];
+                        int profit2 = maxProfit[i2][j2];
+                        maxProfits = Math.max(maxProfits, profit1 + profit2);
+                    }
+                }
+            }
+        }
+        return maxProfits;
+    }
+
+    private static void preComputeMaxProfit() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j <= N - M; j++) {
-                maxHoneyProfit[i][j] = getMaxProfitForSection(i, j);
+                maxProfit[i][j] = getMaxProfitForSection(i, j);
             }
         }
     }
 
     private static int getMaxProfitForSection(int row, int col) {
-        int maxProfit = 0;
-
-        // 부분 집합 생성
+        ArrayList<Integer> profitList = new ArrayList<>();
         for (int subset = 0; subset < (1 << M); subset++) {
-            int sum = 0, profit = 0;
+            int sum = 0;
+            int profit = 0;
             for (int k = 0; k < M; k++) {
                 if ((subset & (1 << k)) != 0) {
-                    sum += honey[row][col + k];
-                    profit += honey[row][col + k] * honey[row][col + k];
+                    sum += map[row][col + k];
+                    profit += map[row][col + k] * map[row][col + k];
                 }
             }
             if (sum <= C) {
-                maxProfit = Math.max(maxProfit, profit);
+                profitList.add(profit);
             }
         }
-
-        return maxProfit;
-    }
-
-    private static int getMaxTotalProfit() {
-        int maxProfit = 0;
-
-        // 두 일꾼의 벌통 조합 선택
-        for (int i1 = 0; i1 < N; i1++) {
-            for (int j1 = 0; j1 <= N - M; j1++) {
-                for (int i2 = i1; i2 < N; i2++) {
-                    for (int j2 = (i1 == i2 ? j1 + M : 0); j2 <= N - M; j2++) {
-                        int profit1 = maxHoneyProfit[i1][j1];
-                        int profit2 = maxHoneyProfit[i2][j2];
-                        maxProfit = Math.max(maxProfit, profit1 + profit2);
-                    }
-                }
-            }
-        }
-
-        return maxProfit;
+        return Collections.max(profitList);
     }
 }

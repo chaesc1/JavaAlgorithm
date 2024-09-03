445b8dda475f2ab54bb9328aea0db79b014e46da
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-09-03 17:00:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 139 ms, Memory: 20,472 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
+/**
+ * 메모리: 20,836 KB, 시간: 129 ms
+ */
 public class Solution {
     static int N, M, C;
     static int[][] map;

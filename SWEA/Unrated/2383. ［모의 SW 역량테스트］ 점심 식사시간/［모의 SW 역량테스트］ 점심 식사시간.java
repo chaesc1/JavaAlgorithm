dda2c9075103fbf40b438f35188dc3d5ccacab68
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-09-04 16:48:45 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 140 ms, Memory: 24,496 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -32,6 +32,7 @@ public class Solution {
     static List<Person> people;
     static List<Door> doors;
     static StringBuilder sb = new StringBuilder();
+    static int minTime;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -55,76 +56,62 @@ public class Solution {
                 }
             }
 
-            int result = solve();
-            sb.append("#").append(tc).append(" ").append(result).append("\n");
+            minTime = Integer.MAX_VALUE;
+            int[] selectedDoors = new int[people.size()];
+            dfs(selectedDoors, 0);
+
+            sb.append("#").append(tc).append(" ").append(minTime).append("\n");
         }
         System.out.println(sb.toString());
     }
 
-    private static int solve() {
-        int numPeople = people.size();
-        int minTime = Integer.MAX_VALUE;
-
-        // 모든 사람마다 두 문의 도착 시간을 계산
-        int[][] timeToDoors = new int[numPeople][2];
-        for (int i = 0; i < numPeople; i++) {
-            Person person = people.get(i);
-            for (int j = 0; j < 2; j++) {
-                timeToDoors[i][j] = person.getDistanceTo(doors.get(j));
-            }
-        }
-
-        // 모든 가능한 배치를 시뮬레이션하여 최소 시간을 계산
-        for (int i = 0; i < (1 << numPeople); i++) {
+    private static void dfs(int[] selectedDoors, int personIdx) {
+        if (personIdx == people.size()) {
             List<Integer> door1Times = new ArrayList<>();
             List<Integer> door2Times = new ArrayList<>();
 
-            // 각 배치에 따른 도착 시간을 도출
-            for (int j = 0; j < numPeople; j++) {
-                if ((i & (1 << j)) == 0) {
-                    door1Times.add(timeToDoors[j][0]);
+            for (int i = 0; i < selectedDoors.length; i++) {
+                if (selectedDoors[i] == 0) {
+                    door1Times.add(people.get(i).getDistanceTo(doors.get(0)));
                 } else {
-                    door2Times.add(timeToDoors[j][1]);
+                    door2Times.add(people.get(i).getDistanceTo(doors.get(1)));
                 }
             }
-            minTime = Math.min(minTime, calculateTime(door1Times, doors.get(0).time, door2Times, doors.get(1).time));
+
+            int time = Math.max(calculateDoorTime(door1Times, doors.get(0).time), calculateDoorTime(door2Times, doors.get(1).time));
+            minTime = Math.min(minTime, time);
+
+            return;
         }
 
-        return minTime+1;
+        selectedDoors[personIdx] = 0;
+        dfs(selectedDoors, personIdx + 1);
+
+        selectedDoors[personIdx] = 1;
+        dfs(selectedDoors, personIdx + 1);
     }
 
-    // 각 문의 대기 시간에 대한 최종 시간을 계산
-    private static int calculateTime(List<Integer> door1Times, int door1Time, List<Integer> door2Times, int door2Time) {
-        Collections.sort(door1Times);
-        Collections.sort(door2Times);
-
+    private static int calculateDoorTime(List<Integer> arrivalTimes, int time) {
+        Collections.sort(arrivalTimes);
+        Queue<Integer> queue = new LinkedList<>();
         int totalTime = 0;
-        totalTime = Math.max(totalTime, calculateDoorTime(door1Times, door1Time));
-        totalTime = Math.max(totalTime, calculateDoorTime(door2Times, door2Time));
-
-        return totalTime;
-    }
-
-    // 한 문의 대기 시간 계산
-    private static int calculateDoorTime(List<Integer> arrivalTimes, int stairTime) {
-        int time = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for (int arrival : arrivalTimes) {
             while (!queue.isEmpty() && queue.peek() <= arrival) {
                 queue.poll();
             }
             if (queue.size() < 3) {
-                queue.add(arrival + stairTime);
+                queue.add(arrival + time);
             } else {
                 int nextFreeTime = queue.poll();
-                queue.add(nextFreeTime + stairTime);
+                queue.add(nextFreeTime + time);
             }
         }
+
         while (!queue.isEmpty()) {
-            time = queue.poll();
+            totalTime = queue.poll();
         }
 
-        return time;
+        return totalTime + 1;
     }
 }

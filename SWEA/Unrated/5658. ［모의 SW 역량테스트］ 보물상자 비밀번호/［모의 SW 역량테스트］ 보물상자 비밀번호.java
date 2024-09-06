a121b7c963c38c461f80aa73d904c1ed827f0d05
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-09-06 15:23:51 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 115 ms, Memory: 20,820 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -2,43 +2,45 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+//27,492 kb
+//197 ms
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
+
         int TC = Integer.parseInt(br.readLine());
-        
+
         for (int tc = 1; tc <= TC; tc++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
             String input = br.readLine();
-            
+
             int length = N / 4;
-            
+
             // 모든 회전 상태의 숫자를 저장할 세트
             Set<Integer> uniqueNumSet = new HashSet<>();
-            
+
             for (int i = 0; i < length; i++) {
                 for (int j = 0; j < 4; j++) {
                     String num = input.substring(j * length, (j + 1) * length);
                     uniqueNumSet.add(Integer.parseInt(num, 16));
                 }
-                
+
                 input = input.substring(N - 1) + input.substring(0, N - 1);
             }
-            
+
             // 숫자를 저장한 세트를 정렬된 리스트로 변환
             List<Integer> sortedNumList = new ArrayList<>(uniqueNumSet);
-            Collections.sort(sortedNumList, Collections.reverseOrder());
-            
+            sortedNumList.sort(Collections.reverseOrder());
+
             // 상위 K번째 값을 얻음
             int result = sortedNumList.get(K - 1);
-            
+
             sb.append("#").append(tc).append(" ").append(result).append("\n");
         }
-        
+
         System.out.println(sb.toString());
     }
 }

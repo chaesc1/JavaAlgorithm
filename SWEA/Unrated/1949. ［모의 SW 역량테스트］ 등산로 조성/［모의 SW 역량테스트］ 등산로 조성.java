868ea53a70338dc71a772fad5b121f7e32069823
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-09-06 14:36:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 110 ms, Memory: 18,424 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -3,39 +3,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int N, K,answer;
-    static int[][] board;
+    static int N, K;
+    static int[][] map;
     static boolean[][] visited;
-    static int maxHeight;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static int maxHeight;
+    static int answer;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int testcase = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= testcase; tc++) {
+        int TC = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= TC; tc++) {
             st = new StringTokenizer(br.readLine());
+
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            board = new int[N][N];
+
+            map = new int[N][N];
             visited = new boolean[N][N];
 
             maxHeight = 0;
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    board[i][j] = Integer.parseInt(st.nextToken());
-                    maxHeight = Math.max(maxHeight, board[i][j]);
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    if (map[i][j] > maxHeight) {
+                        maxHeight = map[i][j];
+                    }
                 }
             }
+
             answer = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (board[i][j] == maxHeight) {
-                        // i,j 에서부터 깊이우선 탐색으로 가장 긴 거리 구하면 되는 문제
+                    if (map[i][j] == maxHeight) {
+                        // 가장 높은 곳에서 부터 탐색
                         visited[i][j] = true;
                         dfs(i, j, maxHeight, 1, 0);
                         visited[i][j] = false;
@@ -44,34 +50,44 @@ public class Solution {
             }
             sb.append("#").append(tc).append(" ").append(answer).append("\n");
         }
+
         System.out.println(sb.toString());
     }
 
-    //긴 등산로를 만들기 위해 딱 한 곳을 정해서 최대 K 깊이만큼 지형을 깎는 공사를 할 수 있다.
-    private static void dfs(int x, int y, int cur, int len, int count) {
-        answer = Math.max(answer, len);
+    /**
+     * (x, y)에서 시작하여 가능한 가장 긴 등산로를 찾기 위한 깊이 우선 탐색(DFS).
+     *
+     * @param x      현재 x 좌표
+     * @param y      현재 y 좌표
+     * @param height 현재 높이
+     * @param length 현재 등산로 길이
+     * @param count  지형을 깎은 횟수
+     */
+    private static void dfs(int x, int y, int height, int length, int count) {
+        answer = Math.max(answer, length);
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            if (visited[nx][ny]) continue;
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
 
-            if (board[nx][ny] < cur) {
+            // 다음 지점이 현재 지접 보다 낮은경우 dfs 깊이 우선 탐색 수행
+            if (map[nx][ny] < height) {
                 visited[nx][ny] = true;
-                dfs(nx, ny, board[nx][ny], len + 1, count);
+                dfs(nx, ny, map[nx][ny], length + 1, count);
                 visited[nx][ny] = false;
             } else {
-                // 한번도 깎지 않았으면
+                // 한번도 깎지 않았고 등산로를 만들 수 있는 경우
                 if (count == 0) {
-                    if (board[nx][ny] - K < cur) {
+                    if (map[nx][ny] - K < height) {
                         visited[nx][ny] = true;
-                        dfs(nx, ny, cur - 1, len + 1, count + 1);
+                        dfs(nx, ny, height - 1, length + 1, count + 1);
                         visited[nx][ny] = false;
                     }
                 }
             }
         }
+
     }
 }
